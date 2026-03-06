e28eddf82fab5479d5e29652b3b47115dfc3d7ca
shaman2016scratch <g.ilkh@yandex.ru>
2026-03-06 17:02:51 +0300

    UPDATE

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,10 @@ import App from "./App.jsx";
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<App />
+		<footer>
+            <div class="container">
+                <p class="footer-text">© 2026, BluOf all rights reserved</p>
+            </div>
+        </footer>
 	</StrictMode>,
 );
