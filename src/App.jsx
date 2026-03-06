cbf7d2ec8d58ad46e56f81546f899ba552df797e
shaman2016scratch <g.ilkh@yandex.ru>
2026-03-06 09:07:06 +0300

    Start

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const App = () => {
+	return ();
+};
+
+export default App;
---
+++ src/components/login/login.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./login.css";
+
+const Login = (props) => {
+	const [input, setInput] = useState("");
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+	return ();
+};
+
+export default Login;
