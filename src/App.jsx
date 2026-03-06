a7bd6c45720a8c0490eb7008870d226423647871
shaman2016scratch <g.ilkh@yandex.ru>
2026-03-06 09:17:44 +0300

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import Regestrate from "./components/reg/reg.jsx";
+import Login from "./components/login/login.jsx"; 
 import "./App.css";
 
 const App = () => {
