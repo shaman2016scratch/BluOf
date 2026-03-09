c8b5beb7fe24fa9056e10b2c5c0c2ccb4c9d205b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-09 21:46:23 +0300

    Update tp.jsx

--- src/components/tp/tp.jsx
+++ src/components/tp/tp.jsx
@@ -1,1 +1,10 @@
+import { useState } from "react";
 
+const Main = (props) => {
+	const [input, setInput] = useState("");
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+	return ();
+};
+
+export default Main;
