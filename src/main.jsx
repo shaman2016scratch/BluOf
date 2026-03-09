454da58b9909645adf9f6b98f7ca8b348fb8fbd7
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-09 21:49:08 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import yourInTopPanel from "./components/yourInTopPanel/yourInTopPanel.jsx";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -8,20 +7,6 @@ let token = localStorage.getItem('bluof-session-token'),
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		{ token ? (
-            <>
-                <a href="https://shaman2016scratch.github.io/BluOf/"><img src='https://shaman2016scratch.github.io/BluOf/bluof-logo.svg' width='25' height='25' /></a>
-                <yourInTopPanel />
-            </>
-        ) : (
-            <>
-                <div class="top-panel">
-				    <a href="https://shaman2016scratch.github.io/BluOf/"><img src='https://shaman2016scratch.github.io/BluOf/bluof-logo.svg' width='25' height='25' /></a>
-				    <a href='/BluOf/login.html'>LOG IN</a>
-			        <a href='/BluOf/reg.html'>REGESTRATION</a>
-                </div>
-            </>
-        )}
 		<App />
 		<footer>
             <div class="container">
