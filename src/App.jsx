de821815232639c5d2a5545797abafe26d882d80
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-09 21:53:22 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Noauth from "./components/noauth/noauth.jsx";
 import Credits from "./components/main/credits.jsx";
 import Login from "./components/login/login.jsx";
 import Regestrate from "./components/reg/reg.jsx";
+import TP from "./components/tp/tp.jsx";
 import { isLogin, getMyUser } from "./lib/user.js";
 import "./App.css";
 
@@ -15,6 +16,7 @@ const App = () => {
 	if (page === "/" || page === "/index.html" || page === "/index.html") {
 	    return (
 		    <div className="app-viewport">
+			    <TP />
 			    { token ? (
 			        <>
 				        <Main></Main>
@@ -28,18 +30,21 @@ const App = () => {
 	} else if (page === "/credits.html" || page === "/credits") {
 		return (
 		    <div className="app-viewport">
+				<TP />
 			    <Credits />
 			</div>
 	    );
 	} else if (page === "/about.html" || page === "/about") {
 		return (
 		    <div className="app-viewport">
+				<TP />
 			    <h1>About BluOf</h1>
 			</div>
 	    );
 	} else if (page === "/login.html" || page === "/login") {
 		return (
 		    <div className="app-viewport">
+				<TP />
 				{ token ? (
 				    <>
 						<h1>ERROR 403</h1>
@@ -55,6 +60,7 @@ const App = () => {
 	} else if (page === "/reg.html" || page === "/reg") {
 		return (
 		    <div className="app-viewport">
+				<TP />
 				{ token ? (
 				    <>
 					    <h1>ERROR 403</h1>
