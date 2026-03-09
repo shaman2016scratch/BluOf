041839225abcc4397f659fbec7976a7dc86dfabb
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-09 21:50:26 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-let token = localStorage.getItem('bluof-session-token'),
-
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<App />
