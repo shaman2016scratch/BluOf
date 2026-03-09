1287cafd7e5799eb4ed1e2f6bc9bed4d461bc58f
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-09 21:47:06 +0300

    Update tp.jsx

--- src/components/tp/tp.jsx
+++ src/components/tp/tp.jsx
@@ -4,7 +4,22 @@ const Main = (props) => {
 	const [input, setInput] = useState("");
 	const [error, setError] = useState(null);
 	const [loading, setLoading] = useState(false);
-	return ();
+	return (
+		{ token ? (
+            <>
+                <a href="https://shaman2016scratch.github.io/BluOf/"><img src='https://shaman2016scratch.github.io/BluOf/bluof-logo.svg' width='25' height='25' /></a>
+                <yourInTopPanel />
+            </>
+        ) : (
+            <>
+                <div class="top-panel">
+				    <a href="https://shaman2016scratch.github.io/BluOf/"><img src='https://shaman2016scratch.github.io/BluOf/bluof-logo.svg' width='25' height='25' /></a>
+				    <a href='/BluOf/login.html'>LOG IN</a>
+			        <a href='/BluOf/reg.html'>REGESTRATION</a>
+                </div>
+            </>
+        )}
+	);
 };
 
 export default Main;
