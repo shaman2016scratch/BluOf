ee95b856253655a31c3b52dc35c674a70dd75687
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-03-12 13:40:59 +0300

    Update tos.jsx

--- src/components/main/tos.jsx
+++ src/components/main/tos.jsx
@@ -14,9 +14,12 @@ const TOS = (props) => {
             <p>2.1. The BluOf service and similar products are provided by the BluOf Foundation as is and in full.</p>
             <p>2.2. The BluOf Foundation has the right to change any feature or service without prior notice</p>
             <h2>3. Usage rules</h2>
-            <p>users are prohibited from</p>
+            <p>Users are prohibited from</p>
             <p>3.1. Use the service to distribute malware and code</p>
             <p>3.2. Creation, use, and distribution of content that violates the law, copyrights, or terms of use/rules of any service provided by the BluOf Foundation.</p>
+			<p>3.3. Host: viruses, malware, or mining code</p>
+			<p>3.4. Attempts to bypass the security policies of the BluOf Foundation or other companies, or to destabilize their operations</p>
+			<p>3.5. Automated data collection (scraping) without official permission</p>
 		</div>
     );
 };
